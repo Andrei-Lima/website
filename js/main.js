c2814097770ced4de6635701173353a21d709fcc
Andrei Lima <102513288+Andrei-Lima@users.noreply.github.com>
2022-12-27 23:56:11 -0300

    gradient
    gradient

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ var alpha = 0.0;
 window.onload = () => {
   setInterval(function() {
     if (alpha < 1.0) {
-      alpha += 0.05;
-      document.getElementById("begin").style = `opacity: ${alpha};`
+      alpha += 0.02;
+      document.getElementById("begin").style = `opacity: ${alpha};`;
+      document.getElementById("dark").style = `background: linear-gradient(45deg, var(--background-color-dark) ${alpha*10}%, #142a41 ${((alpha+.5)*150)}%);`;
     }
   }, 50);
 
