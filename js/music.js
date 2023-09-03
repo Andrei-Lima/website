1ab61acfe0dcf0fbcf44a86c645a2210e8a2515e
Andrei Lima <102513288+Andrei-Lima@users.noreply.github.com>
2023-09-03 16:57:00 -0300

    a lot of musics
    additionsssss

--- js/music.js
+++ js/music.js
@@ -3,18 +3,20 @@ let album_list = [];
 
 //#region ALBUMS
 
-// Nostalgia
+
+// Radio signals
 makeAlbum(
   "radio signals", '../content/img/albums/radiosignals.png', [
 
     makeSong(
-      "globo", "for this one I tried to make a vaporwave track, filled with nostalgia. I would like to sing in some of these songs but my mic quality is trash and my singing skills are not even acceptable, but you gotta start somewhere right? xD -------- also I sampled a song from an advertisement from the 80s. (my first song using samples)",
+      "globo", "for this one I tried to make a vaporwave track, filled with nostalgia. I would like to sing in some of these songs but my mic quality is trash and my singing skills aren't close to acceptable, but you gotta start somewhere right? xD -------- also I sampled a song from an advertisement from the 80s. (my first song using samples)",
       "../content/audio/albums/radiosignals/globo.mp3"
     ),
     
   ]
 );
 
+// Nostalgia
 makeAlbum(
   "nostalgia", '../content/img/albums/nostalgia.png', [
 
@@ -27,22 +29,111 @@ makeAlbum(
       "", "I wanted to make a track that resembles an old, calming game. I think it sounds wholesome <3", 
       "../content/audio/albums/nostalgia/waterfall.mp3"
     ),
+    
+    makeSong(
+      "", "not much to say about this one, sounds sad", 
+      "../content/audio/albums/nostalgia/emotional.mp3"
+    ),
 
   ]
 );
+
+// Economic failure
+makeAlbum(
+  "economic failure - soundtrack", '../content/img/albums/ef.png', [
+
+    makeSong(
+      "", "", 
+      "../content/audio/albums/ef/coordinates.mp3"
+    ),
+
+    makeSong(
+      "", "", 
+      "../content/audio/albums/ef/him calm 2.mp3"
+    ),
+
+    makeSong(
+      "", "", 
+      "../content/audio/albums/ef/bar.mp3"
+    ),
+
+    makeSong(
+      "", "", 
+      "../content/audio/albums/ef/rekognition.mp3"
+    ),
+
+    makeSong(
+      "", "", 
+      "../content/audio/albums/ef/duck2.mp3", true
+    ),
+
+    makeSong(
+      "", "", 
+      "../content/audio/albums/ef/duck.mp33"
+    ),
+
+    makeSong(
+      "", "", 
+      "../content/audio/albums/ef/afterlife.mp3"
+    ),
+
+  ]
+);
+
 
 // WaW
 makeAlbum(
   "we are watching", '../content/img/albums/waw.png', [
 
     makeSong(
-      "", "this is the first time I'm using my real guitar, i like the low/dark tone i got for this one", 
+      "", "this is the first time I'm using my real guitar, I like the low/dark tone I got for this one", 
       "../content/audio/albums/wearewatching/snd3.mp3"
     ),
 
     makeSong(
       "", "adventures", 
       "../content/audio/albums/wearewatching/snd1.mp3"
+    ),
+
+  ]
+);
+
+// Other
+makeAlbum(
+  "other", "../content/img/albums/other.png", [
+
+    makeSong(
+      "", "All of the songs below were made 1-2 years ago, so I don't like them as much as the ones above", ""
+    ),
+
+    makeSong(
+      "sunset", "the bass is distorted for some reason, and the instruments sound like harmonical beehives",
+      "../content/audio/albums/other/sunset.mp3"
+    ),
+    
+    makeSong(
+      "universe", "my first synthwave ever, the bass is drastic",
+      "../content/audio/albums/other/universe.mp3"
+    ),
+    
+    makeSong(
+      "sentimental", "how sad was I?",
+      "../content/audio/albums/other/sentimental.mp3"
+    ),
+    
+    makeSong(
+      "DOWN", "",
+      "../content/audio/albums/other/down.mp3", true
+    ),
+    
+    makeSong(
+      "END", "",
+      "../content/audio/albums/other/end.mp3", true
+    ),
+    
+    makeSong(
+      "OLD", "",
+      "../content/audio/albums/other/old.mp3", true
     ),
 
   ]
@@ -86,6 +177,7 @@ function createAlbum(album) {
 
     var songsDesc = album.songs[i].description;
     var songsAudio = album.songs[i].audio;
+    var songsLoud = album.songs[i].loudAdvice;
 
     var album_songs = document.createElement("div");
     album_songs.className = "album-songs";
@@ -102,13 +194,23 @@ function createAlbum(album) {
     var song_controller = document.createElement("div");
     song_controller.className = "song-controller";
 
+    
+
     var audio = document.createElement("audio");
     audio.controls = true;
     audio.src = songsAudio;
 
+    if (songsLoud) {
+      var p2 = document.createElement("p");
+      p2.classList = "warning";
+      p2.innerHTML = "loud warning";
+      song_box.append(p2);
+    }
+
     song_desc.append(p1);
     song_controller.append(audio);
     song_box.append(song_desc, song_controller);
+
     album_songs.append(song_box);
     music_box.append(album_songs);
 
@@ -118,11 +220,12 @@ function createAlbum(album) {
 }
 
 
-function makeSong(title, desc, audio) {
+function makeSong(title, desc, audio, loudAdvice = false) {
   var obj = {
     title: title,
     description: desc,
     audio: audio,
+    loudAdvice: loudAdvice,
   }
   
   return obj;
@@ -141,6 +244,7 @@ function makeAlbum(title, cover, songs) {
       title: id.title,
       description: id.description,
       audio: id.audio,
+      loudAdvice: id.loudAdvice,
     }
 
     obj.songs.push(song);
