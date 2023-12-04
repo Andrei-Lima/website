a96f176378bb9aba588d71bd2584fb9666df6055
Andrei Lima <102513288+Andrei-Lima@users.noreply.github.com>
2023-12-04 20:05:51 -0300

    sdgs
    dsgdgs

---
+++ content/fovy/js/ducky.js
@@ -0,0 +1,6 @@
+
+var ducky         = document.getElementById("ducky");
+var duckyImg      = document.getElementById("ducky-img");
+
+duckyImg.src = "../content/img/ducky/duckyidle2.gif";
+ducky.style = "left: -500px; transform: scaleX(-1);"
