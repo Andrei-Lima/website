60d7aa84221e598da68ebd3873c0a8b7a1e066e3
Andrei Lima <102513288+Andrei-Lima@users.noreply.github.com>
2024-02-11 21:13:06 -0300

    ai
    ai

--- content/fovy/js/ducky.js
+++ content/fovy/js/ducky.js
@@ -2,5 +2,9 @@
 var ducky         = document.getElementById("ducky");
 var duckyImg      = document.getElementById("ducky-img");
 
+let duckPos = Math.floor(Math.random()*500)+20;
+let side = Math.floor(Math.random()*2)-1;
+if (side == 0) side = 1;
+
 duckyImg.src = "../content/img/ducky/duckyidle2.gif";
-ducky.style = "left: -500px; transform: scaleX(-1);"
+ducky.style = `right: ${duckPos}px; transform: scaleX(${side});`;
---
+++ js/music.js
@@ -0,0 +1,84 @@
+
+const SONG_LIST = document.getElementById("song-list");
+const ALBUM_TITLE = document.getElementById("album-name");
+const ALBUM_COVER = document.getElementById("album-cover");
+
+
+class Song {
+  constructor(songName, src) {
+    this.songName = songName;
+    this.src = src;
+  }
+}
+
+class Album {
+  constructor(albumName, albumCover, songList) {
+    this.albumName = albumName;
+    this.albumCover = albumCover;
+    this.songList = songList;
+  }
+}
+
+let albumList = [];
+
+var ALBUM1 = new Album("We are Watching", "./content/img/waw.png", []);
+ALBUM1.songList.push(new Song("1", "./content/audio/albums/wearewatching/newone.mp3"));
+ALBUM1.songList.push(new Song("2", "./content/audio/albums/wearewatching/nihil.mp3"));
+ALBUM1.songList.push(new Song("3", "./content/audio/albums/wearewatching/snd1.mp3"));
+ALBUM1.songList.push(new Song("4", "./content/audio/albums/wearewatching/snd3.mp3"));
+ALBUM1.songList.push(new Song("5", "./content/audio/albums/wearewatching/two minutes.mp3"));
+albumList.push(ALBUM1);
+
+var ALBUM1 = new Album("Reverendo Fascista", "./content/img/reverendo fascista.png", []);
+ALBUM1.songList.push(new Song("Redundante Mente", "./content/audio/albums/reverendo/Stoner metal.mp3"));
+ALBUM1.songList.push(new Song("Trazedores da Morte", "./content/audio/albums/reverendo/doom metal.wav"));
+ALBUM1.songList.push(new Song("Marte", "./content/audio/albums/reverendo/GUITAR MOOD.mp3"));
+albumList.push(ALBUM1);
+
+let albumIndex = albumList.length-1;
+
+
+document.getElementById("left-album").onclick = () => {
+  if (albumIndex > 0) albumIndex --;
+  update();
+}
+
+document.getElementById("right-album").onclick = () => {
+  if (albumIndex < albumList.length) albumIndex ++;
+  update();
+}
+
+
+function update() {
+  var a = albumList[albumIndex];
+  ALBUM_TITLE.innerHTML = a.albumName;
+  ALBUM_COVER.src = a.albumCover;
+
+  while (SONG_LIST.lastElementChild) {
+    SONG_LIST.removeChild(SONG_LIST.lastElementChild);
+  }
+
+  for (var i = 0; i < a.songList.length; ++i) {
+    createSongElement(a.songList[i].songName, a.songList[i].src);
+  }
+}
+
+function createSongElement(title, src) {
+  var div = document.createElement("div");
+  div.className = "song";
+  var p = document.createElement("p");
+  p.innerHTML = title;
+  var audio = document.createElement("audio");
+  audio.controls = true;
+  var source = document.createElement("source");
+  source.src = src;
+  source.type = "audio/mp3";
+
+  audio.append(source);
+  div.append(p);
+  div.append(audio);
+  SONG_LIST.append(div);
+}
+
+update();
+
