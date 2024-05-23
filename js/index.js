830f3d105219b11805067599198362a749e5531a
Andrei Lima <102513288+Andrei-Lima@users.noreply.github.com>
2024-05-23 19:19:11 -0300

    phone edition
    phone responsiveness

--- js/index.js
+++ js/index.js
@@ -28,14 +28,37 @@ document.getElementById("button1").onclick = () => {
 // variables
 const gravity = 0.1;
 let star_arr = [];
-
+let cat = {x:canvas.width/2, y:canvas.height/2}
+let dx = 1, dy = 1;
+let cat_img = new Image();
+cat_img.src = "./content/img/cat.png";
 
 setInterval(update, 15);
 
 function update() {
-  let offset = 16;
+  let offset = 0;
   canvas.width = window.innerWidth-offset;
   canvas.height = window.innerHeight-offset;
+
+  // Draw cat DVD
+  var cat_spd = 1;
+  var cat_size = 0.25;
+  g.drawImage(cat_img, cat.x, cat.y, (cat_img.width * cat_size), cat_img.height * cat_size);
+
+  cat.x += dx * cat_spd;
+  cat.y += dy * cat_spd;
+
+  var cw = cat_img.width * cat_size;
+  var ch = cat_img.height * cat_size;
+
+  if (cat.x+cw/2 > canvas.width || cat.x-cw/2 < 0) {
+    dx = dx * -1;
+  }
+
+  if (cat.y+ch/2 > canvas.height || cat.y-ch/2 < 0) {
+    dy = dy * -1;
+  }
+
 
   for (var i=0; i<star_arr.length; i++) {
     var s = star_arr[i];
@@ -50,10 +73,13 @@ function update() {
   
 }
 
+cat.x = Math.floor(Math.random() * window.innerWidth);
+cat.y = Math.floor(Math.random() * window.innerHeight);
+
 
 function create_star(val) {
   for (var i = 0; i<val; i++) {
-    var star = new Star(100, 50);
+    var star = new Star(100, 200);
     star.hsp = (Math.random()*3.00)-1.00;
     star.vsp -= Math.random()*4.00;
 
